75c0ac9c8f220daab4811d53b2df5c7d35c74d0e
mrpratama45 <mrpratama45@gmail.com>
2026-09-07 11:29:43 +0700

    feat: complete CRUD - update dan delete

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -15,7 +15,7 @@ const getAllTasks = async (req, res) => {
 
     // filter by status
     if(status){
-      query += ' AND status = ?'
+      query += ' AND status = ?' // artinya query = query + ' AND status = ?'
       params.push(status)
     }
 
@@ -42,6 +42,39 @@ const getAllTasks = async (req, res) => {
     })
   }
 }
+
+// get task by id
+const getTaskById = async (req, res) => {
+  try{
+    const userId= req.user.userId
+    const {id} = req.params
+
+    const [tasks] = await db.query(
+      'SELECT * FROM tasks WHERE id = ? AND user_id = ?',
+      [id, userId]
+    )
+
+    if(tasks.length === 0){
+      return res.status(404).json({
+        status: 'error',
+        message: 'Task tidak ditemukan'
+      })
+    }
+
+    res.json({
+      status: 'success',
+      data: tasks[0]
+    })
+  }
+  catch(error) {
+    console.log('ambil task error: ', error.message)
+    res.status(500).json({
+      status: 'error',
+      message: "terjadi kesalahan server"
+    })
+  }
+}
+
 
 // Buat Task Baru
 const createTask = async (req, res) => {
@@ -121,7 +154,182 @@ const createTask = async (req, res) => {
   }
 }
 
+// update tasks
+const updateTask = async (req, res) => {
+  try{
+    // ========================================
+    // debug-1
+    // console.log("=== debug ===");
+    // console.log("req.params: ", req.params);
+    // console.log("req.body: ", req.body);
+    // ========================================
+
+    const userId = req.user.userId
+    const {id} = req.params
+    const {title, description, status, due_date} = req.body
+
+    // ======================================================
+    // debug-1
+    // console.log("userId: ", userId);
+    // console.log("id: ", id);
+    // console.log("title: ", title);
+    // console.log("description: ", description);
+    // console.log("status: ", status);
+    // console.log("due_date: ", due_date);
+    // console.log("=== end debug ===");
+    // end debug-1
+    // ======================================================
+
+    // cek task ada dan dimiliki user?
+    const [existingTask] = await db.query(
+      'SELECT * FROM tasks WHERE id = ? AND user_id = ?',
+      [id, userId]
+    )
+
+    if(existingTask.length === 0) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'Task tidak ditemukan'
+      })
+    }
+
+    // validasi title jika terkirim
+    if(title !== undefined) {
+      if(title.trim() === '') {
+        return res.status(400).json({
+          status: 'error',
+          message: 'Title tidak boleh kosong'
+        })
+      }
+
+      if(title.length < 3) {
+        return res.status(400).json({
+          status: 'error',
+          message: 'Title minimal 3 karakter'
+        })
+      }
+
+      if(title.length > 50) {
+        return res.status(400).json({
+          status: 'error',
+          message: 'Title maksimal 50 karakter'
+        })
+      }
+    }
+
+    // validasi description jika terkirim
+    if(description !== undefined) {
+      if(description.trim() === '') {
+        return res.status(400).json({
+          status: 'error',
+          message: 'Description tidak boleh kosong'
+        })
+      }
+
+      if(description.length < 3) {
+        return res.status(400).json({
+          status: 'error',
+          message: 'Description minimal 3 karakter'
+        })
+      }
+
+      if(description.length > 255) {
+        return res.status(400).json({
+          status: 'error',
+          message: 'Description maksimal 255 karakter'
+        })
+      }
+    }
+
+    // validasi status jika dikirim
+    const validStatuses = ['pending', 'in_progress', 'completed']
+    if(status !== undefined && !validStatuses.includes(status)) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'Status harus salah satu dari: pending, in_progress, atau completed'
+      })
+    }
+
+    // update task ke database
+    await db.query(
+      `UPDATE tasks
+      SET title = ?, description = ?, status = ?, due_date = ?
+      WHERE id = ? AND user_id = ?`,
+      [
+        title !== undefined ? title : existingTask[0].title ,
+        description !== undefined ? description : existingTask[0].description,
+        status !== undefined ? status : existingTask[0].status,
+        due_date !== undefined ? due_date : existingTask[0].due_date,
+        id,
+        userId
+      ]
+    )
+
+    // ambil task yg sudah di update
+    const [updateTask] = await db.query(
+      'SELECT * FROM tasks WHERE id = ?',
+      [id]
+    )
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Task berhasil di update',
+      data: updateTask[0]
+    })
+
+  }
+  catch(error) {
+    console.log('update task error: ', error.message)
+    res.status(500).json({
+      status: 'error',
+      message: "terjadi kesalahan server"
+    })
+  }
+}
+
+// delete tasks
+const deleteTask = async (req, res) => {
+  try{
+    const userId = req.user.userId
+    const {id} = req.params
+
+    // cek task ada dan dimiliki user?
+    const [existingTask] = await db.query(
+      'SELECT * FROM tasks WHERE id = ? AND user_id = ?',
+      [id, userId]
+    )
+
+    if(existingTask.length === 0) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'Task tidak ditemukan'
+      })
+    }
+
+    // hapus task
+    await db.query(
+      'DELETE FROM tasks WHERE id = ? AND user_id = ?',
+      [id, userId]
+    )
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Task berhasil di hapus'
+    })
+  }
+  catch(error) {
+    console.log('delete task error: ', error.message)
+    res.status(500).json({
+      status: 'error',
+      message: "terjadi kesalahan server"
+    })
+  }
+}
+
 module.exports = {
   getAllTasks,
-  createTask
+  createTask,
+  getTaskById,
+  updateTask,
+  deleteTask
 }
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const authMiddleware = require('../middleware/authMiddleware')
-const {getAllTasks, createTask} = require('../controllers/taskController')
+const {getAllTasks, createTask, getTaskById, updateTask, deleteTask} = require('../controllers/taskController')
 
 const router = express.Router()
 
@@ -10,7 +10,16 @@ router.use(authMiddleware)
 // get
 router.get('/', getAllTasks)
 
+// get by id
+router.get('/:id', getTaskById)
+
 // post
 router.post('/', createTask)
+
+// put
+router.put('/:id', updateTask)
+
+// delete
+router.delete('/:id', deleteTask)
 
 module.exports = router
